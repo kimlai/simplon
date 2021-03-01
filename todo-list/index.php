4e219e40092ffb476813ba8d6a6f5bb420fb46af
kimlai <hello@kimlaitrinh.me>
2021-03-01 16:29:37 +0100

    add todo list

---
+++ todo-list/index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Todo List</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      form {
+        display: inline-block;
+      }
+    </style>
+  </head>
+
+  <body>
+    <main>
+      <form method="POST">
+        <label for="add-task">Nouvelle tâche</label>
+        <input id="add-task" name="todo" />
+        <button>Ajouter</button>
+      </form>
+      <ul>
+        <?php require("./task.php"); ?>
+      </ul>
+    </main>
+  </body>
+</html>
